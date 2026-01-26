06df5c9001f402e813dbce17efd3ea64e880acb6
Adrian Nads Marcelo <marcelonads@gmail.com>
2026-01-27 00:09:03 +0800

    add images to project card

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -4,26 +4,44 @@ type ProjectCardProps = {
 	projectName: string;
 	liveSiteLink: string;
 	repositoryLink: string;
+	imageSrc: string;
 };
+
 const ProjectCard = ({
 	projectName,
 	liveSiteLink,
 	repositoryLink,
+	imageSrc,
 }: ProjectCardProps) => {
 	return (
-		<div className="border border-slate-200 rounded-lg min-w-[340px] p-5 ">
-			<p className="text-lg font-bold text-orange-950">{projectName}</p>
-			<div className="mt-3 flex gap-2">
-				<button className="bg-slate-950 text-white p-2 rounded-lg text-[14px] font-bold font-roboto cursor-pointer">
-					<Link href={liveSiteLink} target="_blank">
-						View Live Site
+		<div className="flex  flex-col overflow-hidden rounded-xl border border-slate-200 bg-white shadow-sm hover:shadow-lg transition-shadow duration-300 min-w-[340px] max-w-[380px]">
+			{/* Image */}
+			<div className="relative h-[200px] w-full">
+				<img src={imageSrc} alt={projectName} className="object-cover" />
+			</div>
+
+			{/* Content */}
+			<div className="flex flex-col gap-4 p-5 flex-1">
+				<p className="text-lg font-bold text-orange-950">{projectName}</p>
+
+				{/* Buttons */}
+				<div className="mt-auto flex gap-3">
+					<Link
+						href={liveSiteLink}
+						target="_blank"
+						className="flex-1 text-center bg-slate-950 text-white py-2 rounded-lg text-sm font-bold hover:bg-slate-800 transition"
+					>
+						Live Site
 					</Link>
-				</button>
-				<button className="bg-slate-950 text-white p-2 rounded-lg text-[14px] font-bold font-roboto cursor-pointer">
-					<Link href={repositoryLink} target="_blank">
-						View Code
+
+					<Link
+						href={repositoryLink}
+						target="_blank"
+						className="flex-1 text-center border border-slate-900 text-slate-900 py-2 rounded-lg text-sm font-bold hover:bg-slate-900 hover:text-white transition"
+					>
+						Code
 					</Link>
-				</button>
+				</div>
 			</div>
 		</div>
 	);
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import ProjectCard from "./components/ProjectCard";
-
 const projects = [
 	{
 		projectName: "Easybank Landing Page",
@@ -7,6 +6,7 @@ const projects = [
 			"https://adriandotdev.github.io/NADS-PROGRAMMER-easybank-landing-page/",
 		repositoryLink:
 			"https://github.com/adriandotdev/NADS-PROGRAMMER-easybank-landing-page",
+		imageSrc: "/projects-images/easy-bank.png",
 	},
 	{
 		projectName: "NFT Preview Card",
@@ -14,6 +14,7 @@ const projects = [
 			"https://adriandotdev.github.io/NADS-PROGRAMMER-NFT-PREVIEW-CARD/",
 		repositoryLink:
 			"https://github.com/adriandotdev/NADS-PROGRAMMER-NFT-PREVIEW-CARD",
+		imageSrc: "/projects-images/nft-preview-card.png",
 	},
 	{
 		projectName: "Order Summary Design",
@@ -21,6 +22,7 @@ const projects = [
 			"https://adriandotdev.github.io/NADS-PROGRAMMER-Order-Summary/",
 		repositoryLink:
 			"https://github.com/adriandotdev/NADS-PROGRAMMER-Order-Summary",
+		imageSrc: "/projects-images/order-summary-design.png",
 	},
 	{
 		projectName: "Stats Preview Design",
@@ -28,6 +30,7 @@ const projects = [
 			"https://adriandotdev.github.io/NADS-PROGRAMMER-stat-preview-design/",
 		repositoryLink:
 			"https://github.com/adriandotdev/NADS-PROGRAMMER-stat-preview-design",
+		imageSrc: "/projects-images/stats-preview-design.png",
 	},
 	{
 		projectName: "Three Column Preview Card",
@@ -35,6 +38,7 @@ const projects = [
 			"https://adriandotdev.github.io/NADS-PROGRAMMER-3-COLUMN-PREVIEW-CARD/",
 		repositoryLink:
 			"https://github.com/adriandotdev/NADS-PROGRAMMER-3-COLUMN-PREVIEW-CARD",
+		imageSrc: "/projects-images/three-column.png",
 	},
 	{
 		projectName: "Base Apparel - Coming Soon Page",
@@ -42,6 +46,7 @@ const projects = [
 			"https://adriandotdev.github.io/NADS-PROGRAMMER-base-apparel-design/",
 		repositoryLink:
 			"https://github.com/adriandotdev/NADS-PROGRAMMER-base-apparel-design",
+		imageSrc: "/projects-images/base-apparel.png",
 	},
 	{
 		projectName: "Profile Card Component",
@@ -49,6 +54,7 @@ const projects = [
 			"https://adriandotdev.github.io/NADS-PROGRAMMER-profile-card-component/",
 		repositoryLink:
 			"https://github.com/adriandotdev/NADS-PROGRAMMER-profile-card-component",
+		imageSrc: "/projects-images/profile-card.png",
 	},
 	{
 		projectName: "Testimonials Grid",
@@ -56,12 +62,14 @@ const projects = [
 			"https://adriandotdev.github.io/NADS-PROGRAMMER-testimonials-grid/",
 		repositoryLink:
 			"https://github.com/adriandotdev/NADS-PROGRAMMER-testimonials-grid",
+		imageSrc: "/projects-images/testimonials-grid.png",
 	},
 	{
 		projectName: "Tip Calculator",
 		liveSiteLink: "https://adriandev-tip-calculator.netlify.app/",
 		repositoryLink:
 			"https://github.com/adriandotdev/FrontendMentor-Tip-Calculator",
+		imageSrc: "/projects-images/tip-calculator.png",
 	},
 	{
 		projectName: "Responsive Product Preview Card",
@@ -69,42 +77,51 @@ const projects = [
 			"https://adriandotdev.github.io/product-preview-card-component/",
 		repositoryLink:
 			"https://github.com/adriandotdev/product-preview-card-component",
+		imageSrc: "/projects-images/responsive-product-card.png",
 	},
 	{
 		projectName: "QR Code Component",
 		liveSiteLink: "https://adriandotdev.github.io/qr-code-component/",
 		repositoryLink: "https://github.com/adriandotdev/qr-code-component",
+		imageSrc: "/projects-images/qr-code-component.png",
 	},
 	{
 		projectName: "Results Summary Component",
 		liveSiteLink: "https://adriandotdev.github.io/results-summary-component/",
 		repositoryLink: "https://github.com/adriandotdev/results-summary-component",
+		imageSrc: "/projects-images/results-summary-component.png",
 	},
 	{
 		projectName: "Blog Preview Card",
 		liveSiteLink: "https://adriandotdev-blog-preview-card.netlify.app/",
 		repositoryLink:
 			"https://github.com/adriandotdev/frontendmentor-blog-preview-card",
+		imageSrc: "/projects-images/blog-preview-card.png",
 	},
 	{
 		projectName: "News Letter Sign-up Form",
 		liveSiteLink: "/newsletter",
 		repositoryLink: "https://github.com/adriandotdev/frontendmentor-projects",
+		imageSrc: "/projects-images/news-letter-form.png",
 	},
 ];
 export default function Home() {
 	return (
-		<main className="p-6">
-			<h1 className="text-4xl font-bold font-roboto text-slate-950 mb-9">
-				Frontend Mentor Projects
-			</h1>
-			<div className="w-full flex flex-col md:flex-row gap-5 justify-stretch flex-wrap">
-				{projects.map((project, index) => (
+		<main className="pb-6">
+			<div className="bg-white shadow-md py-3 mb-6">
+				<div className="mx-auto max-w-7xl px-6 sm:px-8 lg:px-12">
+					<h1 className="text-xl font-bold">Frontend Mentor Projects</h1>
+				</div>
+			</div>
+
+			<div className="max-w-7xl mx-auto flex flex-col flex-wrap gap-5 sm:flex-row md:justify-center lg:justify-start px-6 sm:px-8 lg:px-12">
+				{projects.reverse().map((project, index) => (
 					<ProjectCard
 						key={index}
 						projectName={project.projectName}
 						liveSiteLink={project.liveSiteLink}
 						repositoryLink={project.repositoryLink}
+						imageSrc={project.imageSrc as string}
 					/>
 				))}
 			</div>
